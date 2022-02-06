a6aab267b067d4a227cadc426ebdbdd9e75156f0
Jonathan Ptricio Cevallos Mayorga <jonathancevv@gmail.com>
2022-02-06 08:21:52 -0500

    Se creo la clase: Plan.java y SubCategoria.java

--- src/main/java/uteq/freelancer_mvc/models/SubCategoria.java
+++ src/main/java/uteq/freelancer_mvc/models/SubCategoria.java
@@ -4,13 +4,21 @@ import lombok.Data;
 
 import javax.persistence.*;
 
+
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @Entity
-@Table(name = "subcategorias")
+@Table(name = "sub_categorias")
 public class SubCategoria {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_sub_categoria")
     private int idSubCategoria;
+
+    @NotEmpty
+    @Column(name = "nombre", length = 50)
+    private String nombre;
 
 }
