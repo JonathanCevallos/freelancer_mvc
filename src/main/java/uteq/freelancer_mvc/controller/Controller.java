59c8775f66e59ff70cd1f1529d2e9968d96ba72b
Jonathan cevallos Mayorga <jonathancevv@gmail.com>
2022-02-13 20:18:45 -0500

    Se agregó la relación de image y  Publication.java

--- src/main/java/uteq/freelancer_mvc/controller/Controller.java
+++ src/main/java/uteq/freelancer_mvc/controller/Controller.java
@@ -1,5 +1,6 @@
 package uteq.freelancer_mvc.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Controller {
     @GetMapping
     String menu(){
-        return "otra";
+        return "index";
     }
+
 }
+
